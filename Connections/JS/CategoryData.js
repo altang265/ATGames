209dcdb9308ab4dfe63773d75d1253acf77c872b
Alexander Tang <altang265@gmail.com>
2024-03-31 17:22:16 -0400

    Merged all js files together. Shuffling of the word list is complete.

--- Connections/JS/CategoryData.js
+++ Connections/JS/CategoryData.js
@@ -1,34 +1,32 @@
-// List of Categories
+// // List of Categories
 
-// Each category object should have a name, color for difficulty, four words
-// for the category
+// // Each category object should have a name, color for difficulty, four words
+// // for the category
 
-const categoryOne = {
-    "Category_Name" : "Burger Companies",
-    "Color" : "Yellow",
-    "List_of_words" : ["Kings", "Guys" , "Shack" , "Burger"],
-};
+// const categoryOne = {
+//     "Category_Name" : "Burger Companies",
+//     "Color" : "Yellow",
+//     "List_of_words" : ["Kings", "Guys" , "Shack" , "Burger"],
+// };
 
-const categoryTwo = {
-    "Category_Name" : "Greatest Scientific Minds",
-    "Color" : "Green",
-    "List_of_words" : ["Galileo", "Nikola", "Marie", "Albert"],
-};
+// const categoryTwo = {
+//     "Category_Name" : "Greatest Scientific Minds",
+//     "Color" : "Green",
+//     "List_of_words" : ["Galileo", "Nikola", "Marie", "Albert"],
+// };
 
-const categoryThree = {
-    "Category_Name" : "Famous Actors of 2023",
-    "Color" : "Light Blue",
-    "List_of_words" : ["Pascal", "Winstead", "Armas", "Ortega"],
-};
+// const categoryThree = {
+//     "Category_Name" : "Famous Actors of 2023",
+//     "Color" : "Light Blue",
+//     "List_of_words" : ["Pascal", "Winstead", "Armas", "Ortega"],
+// };
 
-const categoryFour = {
-    "Category_Name" : "Ben and Jerry's Icecream",
-    "Color" : "Purple",
-    "List_of_words" : ["Garcia" , "Monkey", "Food" , "Cheesecake"],
-}
+// const categoryFour = {
+//     "Category_Name" : "Ben and Jerry's Icecream",
+//     "Color" : "Purple",
+//     "List_of_words" : ["Garcia" , "Monkey", "Food" , "Cheesecake"],
+// }
 
-const categoryList = [categoryOne, categoryTwo, categoryThree, categoryFour];
-var connectionsWordList = [];
-for(var cat of categoryList){
-    connectionsWordList.concat(cat.List_of_words);
-}
+// const categoryList = [categoryOne, categoryTwo, categoryThree, categoryFour];
+// var connectionsWordList = categoryOne.List_of_words.concat(categoryTwo.List_of_words, categoryThree.List_of_words, categoryFour.List_of_words);
+// console.log("Here is the list of words for all the categories " + connectionsWordList.toString());
