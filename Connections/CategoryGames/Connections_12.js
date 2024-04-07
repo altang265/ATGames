da0ce4b14e0eeb18bb68ee388d7fcf1d7ffc4ebc
Alexander Tang <altang265@gmail.com>
2024-04-07 12:20:32 -0400

    Added another connections.

---
+++ Connections/CategoryGames/Connections_12.js
@@ -0,0 +1,23 @@
+const categoryOne = {
+    "Category_Name" : "Opposition",
+    "Color" : "Yellow",
+    "List_of_words" : ["Oppose" , "Anti" , "Against", "Contrary"],
+};
+
+const categoryTwo = {
+    "Category_Name" : "Cardiovascular Activities",
+    "Color" : "Green",
+    "List_of_words" : ["Run" , "Cycle" , "Swim" , "Row"],
+};
+
+const categoryThree = {
+    "Category_Name" : "Public Spaces",
+    "Color" : "LightBlue",
+    "List_of_words" : ["Park" , "Street" , "Library" , "Squares"],
+};
+
+const categoryFour = {
+    "Category_Name" : "Blue Collar Jobs",
+    "Color" : "Purple",
+    "List_of_words" : ["Electric" , "Metal" , "Pipe" , "Wood"],
+}
