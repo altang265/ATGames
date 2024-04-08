e0135b65bb484bdbc1c4a57decd7ab6861d0e8d6
Alexander Tang <altang265@gmail.com>
2024-04-07 23:45:27 -0400

    Another Connections done by Andrew

---
+++ Connections/CategoryGames/Connections_Andrew_2.js
@@ -0,0 +1,23 @@
+const categoryOne = {
+    "Category_Name" : "Turning Motions",
+    "Color" : "Yellow",
+    "List_of_words" : ["Rotate" , "Spin" , "Turn" , "Gyrate"],
+};
+
+const categoryTwo = {
+    "Category_Name" : "To Cover",
+    "Color" : "Green",
+    "List_of_words" : ["Shadow" , "Eclipse" , "Block" , "Hide"],
+};
+
+const categoryThree = {
+    "Category_Name" : "Sun-",
+    "Color" : "LightBlue",
+    "List_of_words" : ["Screen" , "Bathe" , "Lotion" , "Tan"],
+};
+
+const categoryFour = {
+    "Category_Name" : "Solar-",
+    "Color" : "Purple",
+    "List_of_words" : ["Panel" , "Flare" , "System" , "Power"],
+}
