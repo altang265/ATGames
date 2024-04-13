46339ffb8383e16e4a8a85a9867e6349bca6ec76
Alexander Tang <altang265@gmail.com>
2024-04-13 11:07:17 -0400

    Working submission feature. Keyboard input and physical keyboard input functionality is complete.

--- Wordle/public/Wordle.js
+++ Wordle/public/Wordle.js
@@ -2,8 +2,11 @@ const keyboardKeys = ["Q", "W" , "E" , "R" , "T" , "Y" , "U", "I" , "O" , "P" ,
     "A" , "S" , "D" , "F" , "G" , "H" , "J" , "K", "L" , "ENTER" , "Z" , "X" , "C" , "V" ,
     "B" , "N" , "M" , "DELETE"];
 
-const wordList = []
-let chosenWord = Math.floor(Math.random() * wordList.length); 
+const wordList = ["HELLO" , "RIGHT" , "LIGHT" , "PATCH" , "TABLE" , "REACH" 
+, "LEECH" , "LATCH" , "PEACH"];
+// let chosenWord = Math.floor(Math.random() * wordList.length); 
+// Make sure everything is in uppercase
+let chosenWord = "HELLO";
 let userSubmissionsList = [];
 let userInputList = [];
 let guessesLeft = 6;
@@ -12,6 +15,10 @@ let guessesLeft = 6;
 document.addEventListener("keydown", (e) => {
     let key = e.key.toUpperCase();
     // console.log(key);
+    keyPressed(key);
+});
+
+function keyPressed(key){
     if(!keyboardKeys.includes(key) && key != "BACKSPACE") return;
     if(key == "BACKSPACE" && userInputList.length > 0){
         addLetterToBoard(key, userInputList.length-1);
@@ -26,13 +33,22 @@ document.addEventListener("keydown", (e) => {
         addLetterToBoard(key, userInputList.length-1);
     }
     if(key == "ENTER" && userInputList.length == 5){
+        // Create a string out of all the characters the user inputted
+        let submission = userInputList.join("");
+        // Check to see if the word is in the wordlist before subtracting a life
+        if(!checkSubmission(submission)){
+            alert("Not in the word list...");
+            return;
+        }
+        // Add the user guess to history
         userSubmissionsList = userSubmissionsList.concat(userInputList);
+        // Empty out the list of characters the user put in
         userInputList.splice(0, userInputList.length);
         console.log("Submission List: " + userSubmissionsList +
-         "\nUser input list check: " + userInputList);
+         "\nUser Submission: " + submission);
         guessesLeft--;
     }
-});
+}
 
 // function that will add the key that was pressed to the document board
 function addLetterToBoard(key, pointer){
@@ -44,26 +60,46 @@ function addLetterToBoard(key, pointer){
         lettersInRow[pointer].innerHTML = key;
 }
 
+// Helper function for checkSubmission() 
+// This function changes the color of the tile 
+function tileColorChange(color , indexOfLetter){
+    // Get the row the user just submitted
+    let inputRow = document.getElementsByClassName("GameGridRow")[6-guessesLeft];
+    let lettersInRow = inputRow.children;
+    lettersInRow[indexOfLetter].style.backgroundColor = color;
+    lettersInRow[indexOfLetter].style.opacity = ".60";
+    lettersInRow[indexOfLetter].style.color = "Black";
+}
+
 // Function that checks the user submission against the word of the day
+// Returns TRUE if the word is in the wordlist
+// Returns FALSE if the word is not in the wordlist
 function checkSubmission(word){
     // Check if word is in word list
     if(!wordList.includes(word)){
         // Alert the user the word is not in the word list
+        
+        return false;
     }
     for(let i in word){
         // Current letter is the same letter as chosen word index
-        // Current letter is not in the word at all
-        // Current letter is in the word (Yellow)
-        if(!chosenWord.includes(word[i])){
-            // 
+        if(word[i] == chosenWord[i]){
+            // Turn the tile green
+            tileColorChange("Green" , i);
         }
-        // 
-        if(word[i] != chosenWord[i]){
-
+        // Current letter is not in the word at all
+        else if(!chosenWord.includes(word[i])){
+            // Leave the background as is
+        }
+        // We know that the letter is in the word from this point forward
+        // Current letter is in the word (Yellow) 
+        else{
+            // Turn the tile yellow
+            tileColorChange("Yellow" , i);
         }
     }
+    return true;
 }
-
 
 function setupGameBoard(){
     // Create the grid for the letters to be entered
@@ -94,6 +130,14 @@ function setupGameBoard(){
         if(keyToAdd == "ENTER" || keyToAdd == "DELETE")
             letterBox.setAttribute("style" , "width: fit-content");
         letterBox.innerHTML = keyToAdd;
+        // Allow the user to click on the keyboard
+        letterBox.addEventListener("click" , () => {
+            console.log("Keyboard pressed: " + keyToAdd);
+            if(keyToAdd != "DELETE")
+                keyPressed(keyToAdd);
+            else 
+                keyPressed("BACKSPACE");
+        });
         keyGridRow.appendChild(letterBox);
         keyGridRow.setAttribute("class" , "GameGridRow");
         if(i == 9 || i == 18 || i == 27){
