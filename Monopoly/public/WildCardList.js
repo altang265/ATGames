25f51ecb02fa323513578b07af89a6cf446ac5be
Alexander Tang <altang265@gmail.com>
2024-05-07 19:59:13 -0400

    More frontend work on monopoly pages

---
+++ Monopoly/public/WildCardList.js
@@ -0,0 +1,29 @@
+export const defaultWildCardList = {
+    "Community Chest" : 
+    [
+        
+        
+    ],
+    "Chance" : 
+    {
+        "PayToBank" : [],
+        "BankPaysPlayer" : [],
+        "CollectMoneyFromBank" : [],
+        "AdvanceToX" : 
+        [
+            "Boardwalk", "Go (Collect $200)",
+            "Illinois Avenue. If you pass Go, collect $200",
+            "St. Charles Place. If you pass Go, collect $200",
+            "the nearest Railroad. If unowned, you may buy it" +
+             "from the Bank. If owned, pay wonder twice the rental" + 
+             "to which they are otherwise entitled",
+            "the nearest Railroad. If unowned, you may buy it" +
+             "from the Bank. If owned, pay wonder twice the rental" + 
+             "to which they are otherwise entitled",
+            "nearest Utility. If unowned, you may buy it from the Bank." +
+             "If owned, throw dice and pay owner a total ten times amount thrown.",
+            
+        ]
+    }  
+    
+}
